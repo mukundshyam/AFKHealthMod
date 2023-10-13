f96244d707207cd14648a53456a476840dea6b69
Mukund Shyam <mukund.shm@gmail.com>
2023-10-13 21:43:24 +0530

    First commit

---
+++ src/main/java/net/mukundshyam/afkhealth/AFKHealthClient.java
@@ -0,0 +1,9 @@
+package net.mukundshyam.afkhealth;
+import net.fabricmc.api.ClientModInitializer;
+
+public class AFKHealthClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
