09661311e81363b85d1576a667b091a70c1a3f7e
Mukund Shyam <mukund.shm@gmail.com>
2023-10-17 15:01:51 +0530

    Added "total counter". Next up -> finishing up by adding speed calculator.

--- src/main/java/net/mukundshyam/afkhealth/item/custom/ActivatorItem.java
+++ src/main/java/net/mukundshyam/afkhealth/item/custom/ActivatorItem.java
@@ -1,38 +1,72 @@
 package net.mukundshyam.afkhealth.item.custom;
 
+import com.sun.jna.platform.unix.solaris.Kstat2StatusException;
 import net.minecraft.block.*;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.EnderChestBlockEntity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.DoubleInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.ObjectUtils;
 
-public class ActivatorItem extends Item {
+public class ActivatorItem extends Item{
+
     public ActivatorItem(Settings settings) {
         super(settings);
     }
 
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        if (!context.getWorld().isClient()) {
-            BlockState blockState = context.getWorld().getBlockState(context.getBlockPos());
+    public ActionResult useOnBlock(ItemUsageContext context)  {
+        if (!context.getWorld().isClient()){
+            BlockPos positionClicked = context.getBlockPos();
             PlayerEntity player = context.getPlayer();
-            if (blockState.getBlock() instanceof ChestBlock || blockState.getBlock() instanceof BarrelBlock || blockState.getBlock() instanceof EnderChestBlock) {
-                assert player != null;
-                outputYes(player);
+            BlockEntity type = context.getWorld().getBlockEntity(positionClicked);
+            assert player != null;
+            if (type instanceof ChestBlockEntity) {
+                int total = 0;
+                for (int num = 0; num <= 26; num++) {
+                    int count = getChestStacks((ChestBlockEntity) type, num);
+                    total = total + count;
+                }
+                String totalVal = String.valueOf(total);
+                player.sendMessage(Text.literal(totalVal + " items") , true);
             }
-            else  {
-                assert player != null;
-                outputNo(player);
+            else if (type instanceof BarrelBlockEntity) {
+                int total = 0;
+                for (int num = 0; num <= 26; num++) {
+                    int count = getBarrelStacks((BarrelBlockEntity) type, num);
+                    total = total + count;
+                }
+                String totalVal = String.valueOf(total);
+                player.sendMessage(Text.literal(totalVal + " items") , true);
+            }
+            else if (type instanceof EnderChestBlockEntity) {
+                player.sendMessage(Text.literal("Doesn't work with ender chests."), true);
+            }
+            else {
+                player.sendMessage(Text.literal("Doesn't work with this block."), true);
             }
         }
         return ActionResult.SUCCESS;
     }
-
-    private void outputYes(PlayerEntity player) {
-        player.sendMessage(Text.literal("Is Chest"), true);
+    public int getChestStacks(ChestBlockEntity chest, int i){
+        ItemStack itemStack = chest.getStack(i);
+        return itemStack.getCount();
     }
-    private void outputNo(PlayerEntity player) {
-        player.sendMessage(Text.literal("Not Chest"), true);
+    public int getBarrelStacks(BarrelBlockEntity barrel, int i){
+        ItemStack itemStack = barrel.getStack(i);
+        return itemStack.getCount();
     }
 }
